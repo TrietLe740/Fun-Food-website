ca68f320f4af1b0b8b13026e5cf80409e6833a82
Vinh <vinh466@gmail.com>
2021-12-20 13:32:53 +0700

    start

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+// Load page
+$(window).on('load', function(event) {
+	$('body').removeClass('preloading');
+	$('.preloader').delay(500).fadeOut('fast');
+});
+
+// Kiểm tra form
+function frmTest(frm) {
+    var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	var phoneReg = /^[0-9]{9,10}$/;
+
+    if ( emailReg.test(frm.email.value ) == false) {
+        alert("Vui lòng nhập địa chỉ email hợp lệ.");
+        frm.email.focus();
+        return false;
+    }
+	if ( phoneReg.test(frm.phone.value ) == false) {
+        alert("Vui lòng nhập số điện thoại hợp lệ.");
+        frm.phone.focus();
+        return false;
+    }
+    if ( frm.name.value.length < 4 ) {
+        alert("Vui lòng nhập tên của bạn");
+    }
+	if ( frm.lname.value.length < 2 ) {
+        alert("Vui lòng nhập họ của bạn");
+    }
+	if ( frm.message.value.length < 4 ) {
+        alert("Nội dung liên hệ quá ngắn, vui lòng nhập từ 10 ký tự trở lên.");
+        frm.message.focus();
+        return false;
+    }
+    alert("Thông tin của bạn đã được gửi đi");
+    return true;
+}
