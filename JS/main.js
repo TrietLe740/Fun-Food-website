1a850b5213d309773c79bedbf9d3dda9d129a92d
Vinh <vinh466@gmail.com>
2021-12-21 13:50:43 +0700

    Final Commit :)

--- JS/main.js
+++ JS/main.js
@@ -9,6 +9,19 @@ function frmTest(frm) {
     var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 	var phoneReg = /^[0-9]{9,10}$/;
 
+    if(frm.lname) {
+            if ( frm.lname.value.length < 2 ) {
+                alert("Vui lòng nhập họ hợp lệ.");
+                frm.lname.focus();
+                return false;
+            }
+        }
+    if ( frm.name.value.length < 4 ) {
+        alert("Vui lòng nhập tên hợp lệ.");
+        frm.name.focus();
+        return false;
+    }
+    console.log('frm.email.value')
     if ( emailReg.test(frm.email.value ) == false) {
         alert("Vui lòng nhập địa chỉ email hợp lệ.");
         frm.email.focus();
@@ -18,12 +31,6 @@ function frmTest(frm) {
         alert("Vui lòng nhập số điện thoại hợp lệ.");
         frm.phone.focus();
         return false;
-    }
-    if ( frm.name.value.length < 4 ) {
-        alert("Vui lòng nhập tên của bạn");
-    }
-	if ( frm.lname.value.length < 2 ) {
-        alert("Vui lòng nhập họ của bạn");
     }
 	if ( frm.message.value.length < 4 ) {
         alert("Nội dung liên hệ quá ngắn, vui lòng nhập từ 10 ký tự trở lên.");
